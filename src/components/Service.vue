e505a7f2147a1f1da3d85feae16a241d179dcfae
1337 <35700046+one-three-three-seven@users.noreply.github.com>
2024-02-28 18:22:35 +0100

    update Coinbase and Kiln

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -40,6 +40,9 @@
         <Button icon="pi pi-minus" class="bg-orange-400 ml-2 self-end rounded-full w-8 h-8 mb-1" :disabled="removeMenuItems.length === 1" @click="toggleRemoveMenu" />
         <Menu ref="removeMenu" :model="removeMenuItems" :popup="true" />
     </div>
+    <template v-if="service.name === 'Coinbase'">
+        <span class="text-base">*Coinbase will reduce their Geth usage to 50% in the coming weeks.</span>
+    </template>
 </template>
 
 <script setup lang="ts">
