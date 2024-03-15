c0da6981d70bb2434d5bb932f9736133e41f1885
1337 <35700046+one-three-three-seven@users.noreply.github.com>
2024-03-15 21:05:47 +0100

    update coinbase, add warning

--- src/components/Service.vue
+++ src/components/Service.vue
@@ -43,6 +43,9 @@
     <template v-if="service.name === 'Coinbase'">
         <span class="text-base">*Coinbase will reduce their Geth usage to 50% in the coming weeks.</span>
     </template>
+    <template v-if="['Kraken', 'OKX', 'Bitcoin Suisse', 'Binance'].includes(service.name)">
+        <span class="text-base text-red-600 font-bold">*Risky! No public data available.</span>
+    </template>
 </template>
 
 <script setup lang="ts">
